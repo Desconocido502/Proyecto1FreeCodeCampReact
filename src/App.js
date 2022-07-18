738c2df43b559a13da3526918562d554808aaad8
Desconocido502 <3018554470101@ingenieria.usac.edu.gt>
2022-07-17 23:00:27 -0600

    Se movieron los datos a un archivo json y desde ese archivo se obtienen todos los datos de los testimonios, asi de una manera dinamica funciona mejor

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import "./App.css";
 import React from "react";
 import Testimonio from "./components/Testimonio";
+import testimonios from "./helpers/Testimonios.json";
 
 function App() {
+  //testimonios.data.forEach((testimonio) => console.log(testimonio));
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio
-          nombre="Emma Bostian"
-          pais="Suecia"
-          imagen="emma"
-          cargo="Ingeniero de software"
-          empresa="Spotify"
-          testimonio="Siempre me ha costado aprender JavaScript. Tomé muchos cursos, 
-          pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así 
-          como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades 
-          y la confianza que necesitaba para conseguir el trabajo de mis sueños como un 
-          ingeniero de software en Spotify."
-        />
-        <Testimonio
-          nombre="Shawn Wang"
-          pais="Singapur"
-          imagen="shawn"
-          cargo="Ingeniero de software"
-          empresa=" Amazon"
-          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía 
-          programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. 
-          En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió 
-          mi vida ."
-        />
-        <Testimonio
-          nombre="Sarah Chima"
-          pais="Nigeria"
-          imagen="sarah"
-          cargo="Ingeniero de software"
-          empresa=" ChatDesk"
-          testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. 
-          El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante 
-          total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador 
-          en una empresa increíble."
-        />
+        {testimonios.data.map((testimonio) => {
+          return (
+            <Testimonio
+              nombre={testimonio.nombre}
+              pais={testimonio.pais}
+              imagen={testimonio.imagen}
+              cargo={testimonio.cargo}
+              empresa={testimonio.empresa}
+              testimonio={testimonio.testimonio}
+              key={testimonio.imagen}
+            />
+          );
+        })}
       </div>
     </div>
   );
